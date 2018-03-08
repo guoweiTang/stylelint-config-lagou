1021d2390513670827f1ec541b6e762f9281419c
Ice <ice@lagou.com>
2018-03-08 19:58:44 +0800

    1.0.0

---
+++ README.md
@@ -0,0 +1,55 @@
+# stylelint-config-lagou
+### Rules
+The following is introduce of **rules**:
+
+#### Spaces or Empty lines
+* [warn : indentation] 缩进为4个空格
+* [warn : max-empty-lines] 最大连续空2行
+* `[error : block-no-empty] 禁止空代码块`
+* `[error : comment-no-empty] 禁止空注释块（多行注释）`
+* [warn : comment-whitespace-inside] 注释两侧必须有空格
+* [warn : value-list-comma-space-before] 逗号前不能有空格
+* [warn : value-list-comma-space-after] 逗号后必须有空格
+* [warn : value-list-max-empty-lines] 禁止值列表中存在空行
+* [warn : declaration-block-semicolon-space-after] 单行语句分号后必须有空格
+* [warn : block-closing-brace-empty-line-before] 语句块（右大括号）结束前一行不能是空行
+* `[error : block-opening-brace-newline-after] 多行语句块（左大括号）后必须换行`
+* [warn : block-opening-brace-space-before] 语句块（左大括号）开始前必须有空格
+* `[error : declaration-colon-space-after] 属性值之前（冒号后）必须有空格`
+* [warn : rule-empty-line-before] 多行规则的语句块间必须有空行
+
+
+
+
+#### Specific Syntax
+* [warn : function-calc-no-unspaced-operator] 禁止calc语句中计算符（+、-、*、/）两侧无空格
+* `[error : declaration-no-important] 禁止!important`
+* `[error : media-feature-name-no-unknown] 禁止未知的media名称`
+* `[error : no-duplicate-at-import-rules] 禁止同一个页面中值重复的@import语句`
+* `[error : no-unknown-animations] 禁止未知的animation-name`
+
+
+#### Structure
+* [warn : selector-max-compound-selectors] 禁止选择器继承关系超过3层
+* `[error : selector-max-id] 禁止ID选择器超过1层`
+* [warn : max-nesting-depth] 禁止选择器嵌套超过3层
+
+#### Others
+* [warn : number-leading-zero] 禁止不足1的小数整数位有0
+* [warn : number-no-trailing-zero] 禁止小数末尾有0
+* [warn : length-zero-no-unit] 禁止数值0后有单位
+* `[error : color-no-invalid-hex] 禁止无效的十六进制颜色`
+* [warn : color-named] 禁止使用颜色名字
+* `[error : string-no-newline] 禁止同一个字符串内部换行（可用\n代替）`
+* [warn : number-max-precision] 禁止小数尾数超过2位
+* `[error : unit-no-unknown] 禁止未知单位`
+* `[error : property-no-unknown] 禁止未知属性`
+* `[error : selector-pseudo-class-no-unknown] 禁止未知伪类选择器`
+* `[error : selector-pseudo-element-no-unknown] 禁止未知伪元素选择器`
+* `[error : declaration-block-no-duplicate-properties] 禁止重复属性名`
+* `[error : no-duplicate-selectors] 禁止重复的选择器`
+* `[error : no-extra-semicolons] 禁止额外的的分号`
+* `[error : declaration-block-trailing-semicolon] 语句必须分号结尾`
+* [warn : string-quotes] 字符串的引号必须为双引号
+* `[error : selector-type-case] 标签选择器必须是小写`
+* `[error : property-case] 属性名小写`
---
+++ index.js
@@ -0,0 +1,164 @@
+/**
+ * @fileoverview Used for exporting Stylelint Configuring.
+ * @author ice.tang
+ */
+'use strict';
+module.exports = {
+    "rules": {
+        "indentation": [
+            4,
+            {
+                "severity": "warning"
+            }
+        ],
+        "max-empty-lines": [
+            2,
+            {
+                "severity": "warning"
+            }
+        ],
+        "block-no-empty": true,
+        "comment-no-empty": true,
+        "comment-whitespace-inside": [
+            "always",
+            {
+                "severity": "warning"
+            }
+        ],
+        "value-list-comma-space-before": [
+            "never",
+            {
+                "severity": "warning"
+            }
+        ],
+        "value-list-comma-space-after": [
+            "always",
+            {
+                "severity": "warning"
+            }
+        ],
+        "value-list-max-empty-lines": [
+            0,
+            {
+                "severity": "warning"
+            }
+        ],
+        "declaration-block-semicolon-space-after": [
+            "always-single-line",
+            {
+                "severity": "warning"
+            }
+        ],
+        "block-closing-brace-empty-line-before": [
+            "never",
+            {
+                "severity": "warning"
+            }
+        ],
+        "block-opening-brace-newline-after": "always-multi-line",
+        "block-opening-brace-space-before": [
+            "always",
+            {
+                "severity": "warning"
+            }
+        ],
+        "declaration-colon-space-after": "always",
+        "rule-empty-line-before": [
+            "always-multi-line",
+            {
+                "severity": "warning",
+                "except": [
+                    "first-nested"
+                ],
+                "ignore": [
+                    "after-comment"
+                ]
+            }
+        ],
+        "function-calc-no-unspaced-operator": [
+            true,
+            {
+                "severity": "warning"
+            }
+        ],
+        "declaration-no-important": true,
+        "media-feature-name-no-unknown": true,
+        "no-duplicate-at-import-rules": true,
+        "no-unknown-animations": true,
+        "selector-max-compound-selectors": [
+            3,
+            {
+                "severity": "warning"
+            }
+        ],
+        "selector-max-id": 1,
+        "max-nesting-depth": [
+            3,
+            {
+                "severity": "warning"
+            }
+        ],
+        "number-leading-zero": [
+            "never",
+            {
+                "severity": "warning"
+            }
+        ],
+        "number-no-trailing-zeros": [
+            true,
+            {
+                "severity": "warning"
+            }
+        ],
+        "length-zero-no-unit": [
+            true,
+            {
+                "severity": "warning"
+            }
+        ],
+        "color-no-invalid-hex": true,
+        "color-named": [
+            "never",
+            {
+                "severity": "warning"
+            }
+        ],
+        "string-no-newline": true,
+        "number-max-precision": [
+            2,
+            {
+                "severity": "warning"
+            }
+        ],
+        "unit-no-unknown": true,
+        "property-no-unknown": [
+            true,
+            {
+                "ignoreProperties": [
+                    "linear-gradient"
+                ]
+            }
+        ],
+        "selector-pseudo-class-no-unknown": true,
+        "selector-pseudo-element-no-unknown": true,
+        "declaration-block-no-duplicate-properties": [
+            true,
+            {
+                "ignoreProperties": [
+                    "background"
+                ]
+            }
+        ],
+        "no-duplicate-selectors": true,
+        "no-extra-semicolons": true,
+        "declaration-block-trailing-semicolon": "always",
+        "string-quotes": [
+            "double",
+            {
+                "severity": "warning"
+            }
+        ],
+        "selector-type-case": "lower",
+        "property-case": "lower"
+    }
+}
